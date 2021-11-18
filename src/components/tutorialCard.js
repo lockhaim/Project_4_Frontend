cf9a632986d3bf0b867c0926dd273f356e768d7f
SoNesky <gueronlj@gmail.com>
2021-11-18 16:49:01 -0500

    pushing to start dev branch

--- src/components/tutorialCard.js
+++ src/components/tutorialCard.js
@@ -13,10 +13,10 @@ const TutorialCard = (props) => {
         <h5>author_id: {guide.author_id}</h5>
         <h5>likes: {guide.likes}</h5>
         <h5>content: {guide.content}</h5>
-        <h5>image: {guide.image}</h5>
+        <img src = {guide.image} alt={guide.name} />
         <h5>rating: {guide.rating}</h5>
- 
-       
+
+
       </div>
     );
   }
