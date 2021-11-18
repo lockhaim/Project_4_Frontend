ecbaa878459f1c31cb946c047f03024fb78e8c6c
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-11-18 12:49:13 -0700

    navbar done

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,32 @@
 const header = () => {
   return(
-    <div>
-        <nav>
-            <div className='guidesLink'>
-                <a className='nav-button-1' href='#'></a>
+    <div className='nav'>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <div className='guidesLink'>
+                    <a className='nav-button nav-button-1' href='#'>Guides</a>
+                </div>
+                <div className='addGuideLink'>
+                    <a className='nav-button nav-button-2' href='#'>Add Guide</a>
+                </div>
+                <div className='compGuidesLink'>
+                    <a className='nav-button nav-button-3' href='#'>Completed Guides</a>
+                </div>
+                <div className='upcGuidesLink'>
+                    <a className='nav-button nav-button-4' href='#'>Upcoming Guides</a>
+                </div>
+                <div className='extResLink'>
+                    <a className='nav-button nav-button-5' href='#'>Resources</a>
+                </div>
+                <div className='login-button'>
+                    <a className='nav-button' href='#'>Login</a>
+                </div>
+                <div className='register-button'>
+                    <a className='nav-button' href='#'>Register</a>
+                </div>
             </div>
-            <div className='addGuideLink'>
-                <a className='nav-button-2' href='#'></a>
-            </div>
-            <div className='compGuidesLink'>
-                <a className='nav-button-3' href='#'></a>
-            </div>
-            <div className='upcGuidesLink'>
-                <a className='nav-button-4' href='#'></a>
-            </div>
-            <div className='extResLink'>
-                <a className='nav-button-5' href='#'></a>
+            <div className='login-links'>
+
             </div>
         </nav>
     </div>
