95533c9dc10a265ade0e228503b2839a193281b2
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-11-18 10:27:26 -0700

    working on header and footer

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,23 @@
 const header = () => {
   return(
     <div>
-
+        <nav>
+            <div className='guidesLink'>
+                <a className='nav-button-1' href='#'></a>
+            </div>
+            <div className='addGuideLink'>
+                <a className='nav-button-2' href='#'></a>
+            </div>
+            <div className='compGuidesLink'>
+                <a className='nav-button-3' href='#'></a>
+            </div>
+            <div className='upcGuidesLink'>
+                <a className='nav-button-4' href='#'></a>
+            </div>
+            <div className='extResLink'>
+                <a className='nav-button-5' href='#'></a>
+            </div>
+        </nav>
     </div>
   );
 }
