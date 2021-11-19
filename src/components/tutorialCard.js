3fa35f0101e277a6727be6bb907529fbb3707ef4
zmonea <Zac.j.monea@gmail.com>
2021-11-19 11:45:17 -0500

    Udating front end

--- src/components/tutorialCard.js
+++ src/components/tutorialCard.js
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from 'react'
+import Edit from './edit'
 
 
 const TutorialCard = (props) => {
-    const {guide, handleDelete, editCar} = props;
+    const {guide, handleDelete, handleUpdate} = props;
     let emptyGuide = { name: '', author_id: '', likes:'', content:'', image: '', rating: '' }
 //   const [guide, setGuide] = useState(emptyGuide)
 
 
     return(
         <div className="person" key={guide.id}>
+        
         <h4>Name: {guide.name}</h4>
         <h5>author_id: {guide.author_id}</h5>
         <h5>likes: {guide.likes}</h5>
         <h5>content: {guide.content}</h5>
         <h5>image: {guide.image}</h5>
-        <h5>rating: {guide.rating}</h5>
- 
+        {/* <img src={guide.image} /> */}
+        
+        <Edit handleUpdate={handleUpdate} id={guide.id}/>
+       <button onClick={handleDelete} value={guide.id}>
+                X
+              </button>
        
       </div>
     );
